7249f9dadcadf5a442213dc1d4efe00c2f154703
nikhil-bk <nikhilbk9148@gmail.com>
2023-09-29 07:06:04 +0530

    webhook route modified

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,18 +1,18 @@
 /* eslint-disable camelcase */
-import type { IncomingHttpHeaders } from 'http';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { WebhookRequiredHeaders } from 'svix';
-import type { WebhookEvent } from '@clerk/nextjs/server';
-import { Webhook } from 'svix';
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action';
-import { NextResponse } from 'next/server';
+import type { IncomingHttpHeaders } from "http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { WebhookRequiredHeaders } from "svix";
+import type { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+import { NextResponse } from "next/server";
 
 type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
-    headers: IncomingHttpHeaders & WebhookRequiredHeaders;
-  };
+  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
+};
 const webhookSecret: string = process.env.WEBHOOK_SECRET;
- 
-export default async function handler(
+
+export default async function Handler(
   req: NextApiRequestWithSvixRequiredHeaders,
   res: NextApiResponse
 ) {
@@ -20,56 +20,60 @@ export default async function handler(
   const headers = req.headers;
   // Create a new Webhook instance with your webhook secret
   const wh = new Webhook(webhookSecret);
- 
+
   let evt: WebhookEvent;
   try {
     // Verify the webhook payload and headers
     evt = wh.verify(payload, headers) as WebhookEvent;
     const eventType = evt.type;
-    if (eventType === 'user.created') {
-  
-      const {id,email_addresses,image_url,username,first_name,last_name}=evt.data
-      const mongoUser=await createUser({
-          clerkId:id,
-          name:`${first_name}${last_name?` ${last_name}`:''}`,
-          username:username!,
-          email:email_addresses[0].email_address,
-          picture:image_url
-  
-  
-      })
-      return NextResponse.json({message:"OK",user:mongoUser})
+    if (eventType === "user.created") {
+      const {
+        id,
+        email_addresses,
+        image_url,
+        username,
+        first_name,
+        last_name,
+      } = evt.data;
+      const mongoUser = await createUser({
+        clerkId: id,
+        name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+        username: username!,
+        email: email_addresses[0].email_address,
+        picture: image_url,
+      });
+      return NextResponse.json({ message: "OK", user: mongoUser });
       // res.status(201).json({});
     }
-     if(eventType === 'user.updated'){
-        const {id,email_addresses,image_url,username,first_name,last_name}=evt.data
-      const mongoUser=await updateUser({
-          clerkId:id,
-          updateData:{
-            name:`${first_name}${last_name?` ${last_name}`:''}`,
-            username:username!,
-            email:email_addresses[0].email_address,
-            picture:image_url
-          },
-          path:`/profile/${id}`
-        
-  
-  
-      })
-      return NextResponse.json({message:"OK",user:mongoUser})
+    if (eventType === "user.updated") {
+      const {
+        id,
+        email_addresses,
+        image_url,
+        username,
+        first_name,
+        last_name,
+      } = evt.data;
+      const mongoUser = await updateUser({
+        clerkId: id,
+        updateData: {
+          name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+          username: username!,
+          email: email_addresses[0].email_address,
+          picture: image_url,
+        },
+        path: `/profile/${id}`,
+      });
+      return NextResponse.json({ message: "OK", user: mongoUser });
     }
 
-    if(eventType === 'user.deleted'){
-        const {id}=evt.data
-      const deletedUser=await deleteUser({clerkId:id!})
-      return NextResponse.json({message:"OK",user:deletedUser})
+    if (eventType === "user.deleted") {
+      const { id } = evt.data;
+      const deletedUser = await deleteUser({ clerkId: id! });
+      return NextResponse.json({ message: "OK", user: deletedUser });
     }
   } catch (_) {
     // If the verification fails, return a 400 error
     return res.status(400).json({});
   }
-
- 
-
 }
- 
