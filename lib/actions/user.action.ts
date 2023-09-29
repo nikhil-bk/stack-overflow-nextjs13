8f2864e8a90b6ce2a5a52b2ecda133cfd7ac808e
nikhil-bk <nikhilbk9148@gmail.com>
2023-09-29 10:35:42 +0530

    Added community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
@@ -62,6 +63,17 @@ export async function deleteUser(params: DeleteUserParams) {
     return deletedUser
 
     // revalidatePath(path)
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+    // const {page=1,pageSize=20,filter,searchQuery}=params
+    const users = await User.find({}).sort({createdAt:-1})
+    return {users};
   } catch (error) {
     console.log(error);
     throw error;
