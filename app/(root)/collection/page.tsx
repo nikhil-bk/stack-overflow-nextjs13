46b894968b4164a5d693a72c6ad9bf1278ef8d17
nikhil-bk <nikhilbk9148@gmail.com>
2023-09-30 17:01:17 +0530

    Added collection page frontend and backend function

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,30 +1,28 @@
 import QuestionCard from "@/components/cards/QuestionCard";
-import HomeFilters from "@/components/home/HomeFilters";
+
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
-import { Button } from "@/components/ui/button";
-import { HomePageFilters } from "@/constants/filters";
-import { getQuestions } from "@/lib/actions/question.actions";
-import Link from "next/link";
+
+import { QuestionFilters } from "@/constants/filters";
+
+import { getSavedQuestion } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
 
 export default async function Home() {
-  const result = await getQuestions({});
+  const { userId } = auth();
+
+  if (!userId) return null;
+  const result = await getSavedQuestion({
+    clerkId: userId,
+  });
 
   return (
     <>
-      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">All Questions</h1> 
-
-        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-            Ask a Question
-          </Button>
-        </Link> 
-      </div> 
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        <LocalSearchbar 
+        <LocalSearchbar
           route="/"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
@@ -33,18 +31,15 @@ export default async function Home() {
         />
 
         <Filter
-          filters={HomePageFilters}
+          filters={QuestionFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
-          containerClasses="hidden max-md:flex"
         />
       </div>
 
-      <HomeFilters />
-
       <div className="mt-10 flex w-full flex-col gap-6">
-        {result.questions.length > 0 ?
+        {result.questions.length > 0 ? (
           result.questions.map((question) => (
-            <QuestionCard 
+            <QuestionCard
               key={question._id}
               _id={question._id}
               title={question.title}
@@ -56,13 +51,15 @@ export default async function Home() {
               createdAt={question.createdAt}
             />
           ))
-          : <NoResult 
-            title="There’s no question to show"
+        ) : (
+          <NoResult
+            title="There’s no question saved to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
-          />}
+          />
+        )}
       </div>
     </>
-  )
+  );
 }
